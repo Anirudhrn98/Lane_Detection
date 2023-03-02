2cc4ec681fb49a313f24904cd36eb074bb3ac446
Anirudh Ramesh <anirudhramesh@anirudhs-mbp.wireless.rit.edu>
2023-03-02 13:15:00 -0500

    2nd commit

---
+++ build/CMakeFiles/PROJECT.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROJECT.dir/lane_detection.cpp.o"
+  "CMakeFiles/PROJECT.dir/lane_detection.cpp.o.d"
+  "PROJECT"
+  "PROJECT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PROJECT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
